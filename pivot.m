aa85c6dc9182e34acfc627ed45680bedbbed3886
andrew3690 <42048231+andrew3690@users.noreply.github.com>
2022-10-06 09:57:05 -0300

    Add files via upload

---
+++ pivot.m
@@ -0,0 +1,31 @@
+## Author: 14736466774 <14736466774@LIICT322348>
+## Created: 2022-10-06
+
+function retval = pivot (A,i)
+# quantidade de linhas da matrix
+l = rows(A)
+# pivo da diagonal principal da matrix
+pivo =A(i,i);
+# por default o valor máximo é reconhecido como o pivot
+max = pivo;
+# indice do maior elemento
+max_idx = i;
+# loop que percorre a matriz
+for j = i+1:l
+  # se o valor da diagonal principal for menor que um outro valor da coluna...
+  if A(j,i) > max
+      #...realiza-se a troca do pivo da coluna pelo valor máximo desta
+      max = A(j,i);
+      # novo indice de maior elemento
+      max_idx = j;
+  endif
+endfor
+
+# variável auxiliar para realizar a troca dos valores, obtendo a linha do valor de pivot maior
+aux = A(max_idx, :);
+# coluna de indice que contém o pivot antigo, realiza a troca com a matrix de pivot maior
+A(max_idx, :) = A(i, :);
+# Novo indice é obtido, indice da diagonal principal recebe o valor da coluna de pivot maior
+A(i,:) = aux
+
+endfunction
---
+++ prodscalar.m
@@ -0,0 +1,32 @@
+## Copyright (C) 2022 14736466774
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{retval} =} prodscalar (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: 14736466774 <14736466774@LIICT322348>
+## Created: 2022-10-06
+
+function retval = prodscalar (u,v)
+n = length(u)
+total = 0.0
+  for i = 1:n
+    total =+ u(i) * v(i);
+  endfor
+  retval = total;
+endfunction
